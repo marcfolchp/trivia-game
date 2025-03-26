34e9bb6e83fea73411279520b0472157251a9972
marcfolchp <marcfolchpomares@gmail.com>
2025-03-26 23:56:58 +0100

    a

--- functions.py
+++ functions.py
@@ -9,9 +9,9 @@ import os
 
 # --------- CREDENTIALS ---------
 
-db_user = st.secrets["MONGO"]["DB_USER"]
-db_pass = st.secrets["MONGO"]["DB_PASS"]
-api_key = st.secrets["MONGO"]["API_KEY"]
+db_user = st.secrets["db_user"]
+db_pass = st.secrets["db_pass"]
+api_key = st.secrets["api_key"]
 
 # --------- GAME ---------
 
